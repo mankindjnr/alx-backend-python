0e8f5c8ad98c7faa56e97f467b729b13f256c9be
mankindjnr <kamos@stu.kemu.ac.ke>
2023-07-06 12:16:16 +0300

    math.floor import

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
+import math
 """type-annotated function floor which takes a float n as argument and returns the floor of the float.
 
 """
-
-
 def floor(n: float) -> int:
     """return the largest integer that is less that or equal to the float value"""
-    integer, decimal = str(n).split(".")
-    integer = int(integer)
-    if (n > 0):
-        return integer
-    else:
-        integer += - 1
-        return integer
+    return(math.floor(n))
