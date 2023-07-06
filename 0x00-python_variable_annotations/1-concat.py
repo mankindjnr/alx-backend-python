75623410ff9f8e7cd8223b9655593f409445e912
mankindjnr <kamos@stu.kemu.ac.ke>
2023-07-06 12:21:52 +0300

    pycodestyle fixed

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-type-annotated function concat that takes a string str1 and a string str2 as 
+type-annotated function concat that takes a string str1 and a string str2 as
 arguments and returns a concatenated string
-
 """
 
 
