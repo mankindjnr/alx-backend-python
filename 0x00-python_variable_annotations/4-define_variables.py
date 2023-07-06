21c3fd616b100b69ef54163a2274f11a69166e64
mankindjnr <kamos@stu.kemu.ac.ke>
2023-07-06 12:44:59 +0300

    define variables with annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Define and annotate the following variables
+with the specified values
+"""
+
+
+a: int
+pi: float
+i_understand_annotations: bool
+school: str
+
+a = 1
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
