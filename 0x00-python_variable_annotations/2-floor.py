074241d7602871f5a9a80f969ca9c8fd6fd42033
mankindjnr <kamos@stu.kemu.ac.ke>
2023-07-06 12:18:39 +0300

    pycodestyle fix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import math
-"""type-annotated function floor which takes a float n as argument and returns the floor of the float.
-
 """
+type-annotated function floor which takes a float n as
+argument and returns the floor of the float.
+"""
+
+
 def floor(n: float) -> int:
-    """return the largest integer that is less that or equal to the float value"""
-    return(math.floor(n))
+    """
+    return the largest integer that is less that or equal to the float value
+    """
+    return (math.floor(n))
