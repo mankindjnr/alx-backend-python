3565e83f835101f02845204e42a4fe137e9aa8ca
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-07-10 11:57:51 +0300

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+##!/usr/bin/env python3
+"""
+checking on th type of a return
+"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) ->asyncio.Task[int]:
+    """the return type of a ascio.task"""
+    task: asyncio.Task[int] = asyncio.create_task(wait_random())
+    return task
