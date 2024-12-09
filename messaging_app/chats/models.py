b422a95d9620acf7c8730204e2b217b536220a82
mankindjnr <kamos8646@stu.kemu.ac.ke>
2024-12-09 14:48:07 +0300

    user conversation message models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    user_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    # override default fields from abstract user
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharFiled(max_length=255, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False, db_index=True)
+
+    # custome fields
+    password_hash = models.CharField(max_length=255, null=False, blank=False)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+
+    # Role field as a ENUM
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='guest',
+        null=False,
+        blank=False
+    )
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Remove unused fields from AbstractUser
+    username = None
+    is_staff = None # if you don't need staff functionality
+    is_superuser = None #if you don;t need superuser functionality
+
+    # Authentication field
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str_(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    # Foreign key to User model
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='conversations',
+        blank=False
+    )
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+class Message(models.Model):
+    message_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name='messages',
+        blank=False
+    )
+    # Foreign key to User model
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='messages_sent',
+        blank=False
+    )
+    # Message content
+    message_body = models.TextField(blank=False, null=False)
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.message_id} from {self.sender} in {self.conversation}"
