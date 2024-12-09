6901ceead4ef49568c5b5e1ed6a2d10ec67aeef2
mankindjnr <kamos8646@stu.kemu.ac.ke>
2024-12-09 17:07:42 +0300

    serialize with django

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,9 +2,22 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    email = serializers.CharField(validators=[self.validate_email])
+
     class Meta:
         model = User
         fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    @staticmethod
+    def validate_email(value):
+        email = value
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = serializers.StringRelatedField()
