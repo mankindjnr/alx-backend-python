4902094f31f97a73929ab2155563f4c42d0e0b53
mankindjnr <kamos8646@stu.kemu.ac.ke>
2024-12-09 16:59:45 +0300

    serialization

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from . import models
+from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.User
+        model = User
         fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at']
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -11,7 +11,7 @@ class MessageSerializer(serializers.ModelSerializer):
     conversation = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
-        model = models.Message
+        model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
 
 # StringRelatedField returns the string representation of the related field
@@ -20,5 +20,5 @@ class ConversationSerializer(serializers.ModelSerializer):
     messages = MessageSerializer(many=True, read_only=True) #nested messages
 
     class Meta:
-        model = models.Conversation
+        model = Conversation
         fields = ['conversation_id', 'participants', 'messages', 'created_at']
