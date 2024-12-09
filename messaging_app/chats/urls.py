8a54bd72c77330159fd7fe57ac7ad36f573c5e6c
mankindjnr <kamos8646@stu.kemu.ac.ke>
2024-12-09 17:29:33 +0300

    urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,9 +3,14 @@ from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
+
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+# Create a nested router for messages under conversations
+nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
